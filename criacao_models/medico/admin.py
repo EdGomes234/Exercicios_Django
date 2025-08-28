22746961ad90a7dcc8c8af6ccb747249cdab6042
ed_gomes <edgargomesoliveira448@gmail.com>
2025-08-28 16:03:16 -0300

    Atividade 7.0 Criação de Models

---
+++ criacao_models/medico/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Medico, Especialidade
+# Register your models here.
+
+class MedicoAdmin(admin.ModelAdmin):
+    fields = [
+        "nome",
+        "endereco",
+        "telefone",
+        "email",
+        "data_nascimento",
+        "crm",
+        "id_especialidade"
+    ]
+
+class EspecialidadeAdmin(admin.ModelAdmin):
+    fields = [
+        "nome",
+        "descricao"
+    ]
+
+admin.site.register(Medico, MedicoAdmin)
+admin.site.register(Especialidade, EspecialidadeAdmin)
---
+++ criacao_models/medico/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Medico(models.Model):
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+    crm = models.CharField(max_length=20)
+    id_especialidade = models.ForeignKey('Especialidade', on_delete=models.CASCADE)
+
+
+class Especialidade(models.Model):
+    nome = models.CharField()
+    descricao = models.TextField()
